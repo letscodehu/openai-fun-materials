f9b42ee9e3b539a2301942c663cb2d54747d3323
Krisztian Papp <fejlesztes@letscode.hu>
2024-03-18 09:00:41 +0100

    openai-6

--- createindex.js
+++ createindex.js
@@ -1,13 +1,29 @@
-const lunr = require("lunr")
 const fs = require("fs")
-const rawContent = fs.readFileSync("content.txt").toString().split("\n").filter(s => s != "")
+require("dotenv").config()
 
-const content = rawContent.map((c, i) => { return { id: i, "content": c } })
-const idx = lunr(function() {
-    this.ref("id")
-    this.field("content")
-    content.forEach((doc) => {
-        this.add(doc)
-    }, this)
-})
-fs.writeFileSync("index.json", JSON.stringify(idx))
+const OpenAI = require('openai')
+const pg = require('pg');
+const pgvector = require('pgvector/pg');
+
+async function index() {
+    const client = new pg.Client({ database: 'postgres', password: 'example', user: 'postgres' });
+    await client.connect();
+
+    await client.query('CREATE EXTENSION IF NOT EXISTS vector');
+    await pgvector.registerType(client);
+
+    await client.query('DROP TABLE IF EXISTS documents');
+    await client.query('CREATE TABLE documents (id bigserial PRIMARY KEY, content text, embedding vector(1536))');
+
+    const input = fs.readFileSync("content.txt").toString().split("\n").filter(s => s != "")
+
+    const openai = new OpenAI();
+    const response = await openai.embeddings.create({ input: input, model: 'text-embedding-3-small' });
+    const embeddings = response.data.map((v) => v.embedding);
+
+    for (let [i, content] of input.entries()) {
+        await client.query('INSERT INTO documents (content, embedding) VALUES ($1, $2)', [content, pgvector.toSql(embeddings[i])]);
+    }
+}
+
+index()
